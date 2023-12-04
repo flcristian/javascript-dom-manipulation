ceffe5330a7c41867979e7fc6109ab7c1bffbb22
flcristian <qflorescucristian@gmail.com>
2023-12-04 19:39:56 +0200

    Added hide unconfirmed

---
+++ invite-list/script/data.js
@@ -0,0 +1,44 @@
+let data = [
+    {
+        id: 1,
+        name: "George",
+        confirmed: false
+    },
+    {
+        id: 2,
+        name: "Marian",
+        confirmed: false
+    },
+    {
+        id: 3,
+        name: "Paul",
+        confirmed: true
+    },
+    {
+        id: 4,
+        name: "Marius",
+        confirmed: true
+    },
+    {        
+        id: 5,
+        name: "Andrei",
+        confirmed: false
+    },
+    {        
+        id: 6,
+        name: "Sergiu",
+        confirmed: true
+    },
+    {        
+        id: 7,
+        name: "Silviu",
+        confirmed: false
+    },
+    {        
+        id: 8,
+        name: "Maria",
+        confirmed: true
+    }
+]
+
+let newId = 9;
